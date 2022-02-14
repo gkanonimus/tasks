13421f337a87df8039a6bf83ad441ce8b374b467
gkanonimus <kiryanov.ilya@bk.ru>
2022-02-14 21:09:23 +0300

    Create 6.4.2b

---
+++ 6 module/6.4.2b.c
@@ -0,0 +1,46 @@
+/* Describe a function that determines the index of the first element of an integer array of n elements, the value of which is equal to a given number x. If there is no specified value in the array, the function returns -1. When solving a problem, consider options:
+b) array elements are sorted in ascending order (use binary search algorithm) */
+
+#include <stdio.h>
+int func_2(int *array, int x, int n);
+
+int main()
+{
+    int x, n;
+    scanf("%d", &x);
+    scanf("%d", &n);
+    int array[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &array[i]);
+    }
+    printf("%d\n", func_2(array, x, n));
+}
+
+int func_2(int *array, int x , int n)
+{
+    int low, middle, high;
+    low  = 0;
+    high = n - 1;
+    
+    while (low <= high)
+    {
+        middle = (low + high) / 2;
+        if (array[middle] < x)
+        {
+            low = (low + high) / 2;
+        }
+        else
+        {
+            if (array[middle] > x)
+            {
+                high /= 2;
+            }
+            else
+            {
+                return middle;
+            }
+        }
+    }
+    return -1;
+}
