ea90f2a10689aba85efec4d09117e966b7b42c5d
gkanonimus <kiryanov.ilya@bk.ru>
2022-05-17 14:07:44 +0300

    10.1.5

---
+++ 10 module/10.1.5.c
@@ -0,0 +1,56 @@
+/* Describe a function copy(L) that builds a copy of the list L and returns
+a pointer to the start of a new list */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct listnode 
+{
+    int elem;
+    struct listnode *next;
+} *L, *p;
+
+struct listnode * copy(struct listnode *L)
+{
+    struct listnode *q, *R;
+    q = malloc(sizeof(struct listnode));
+    R = q;
+    while (L != NULL)
+    {
+        q->next = malloc(sizeof(struct listnode));
+        q->elem = L->elem;
+        L = L->next;
+        q = q->next;
+    }
+    return R;
+}
+
+int main()
+{
+    p = malloc(sizeof(struct listnode));
+    p->elem = 5;
+    
+    p->next = malloc(sizeof(struct listnode));
+    p->next->elem = 8;
+    L = p;
+    p = p->next; 
+
+    p->next = malloc(sizeof(struct listnode));
+    p->next->elem = 3;
+    p = p->next;
+    
+    p->next = malloc(sizeof(struct listnode));
+    p->next->elem = 4;
+    p = p->next;
+    
+    p->next = malloc(sizeof(struct listnode));
+    p->next->elem = 7;
+    p->next->next = NULL;
+    
+    struct listnode * R = copy(L);
+    while (R->next != NULL)
+    {
+        printf("%d\n", R->elem);
+        R = R->next;
+    }
+}
