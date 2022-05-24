cb7672985dab29e4f541a4f5b7fe451966bc3093
gkanonimus <kiryanov.ilya@bk.ru>
2022-05-24 13:32:29 +0300

    10.1.9

---
+++ 10 module/10.1.9.c
@@ -0,0 +1,84 @@
+/* Describe the filter(L1, L2) function that removes from the list L1 all the numbers that are contained in the list L2 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct listnode 
+{
+    int elem;
+    struct listnode *next;
+};
+
+bool inside(int number, struct listnode *R2)
+{
+    while (R2 != NULL)
+    {
+        if (number == R2->elem)
+        {
+            return true;
+        }
+        R2 = R2->next;
+    }
+    return false;
+}
+
+struct listnode * filter(struct listnode *L, struct listnode *R)
+{
+    struct listnode *ret, *ret2, *R2;
+    ret = malloc(sizeof(struct listnode));
+    ret2 = ret;
+    
+    while (L != NULL)
+    {
+        if (!inside(L->elem, R))
+        {
+            ret->elem = L->elem;
+            ret->next = malloc(sizeof(struct listnode));
+            ret       = ret->next;
+            R         = R->next;
+        }
+        L = L->next;
+    }
+    ret->next = NULL;
+    return ret2;
+}
+
+int main()
+{
+    struct listnode *L, *p, *R, *q;
+    p = malloc(sizeof(struct listnode));
+    L = p;
+    p->elem = 5;
+    p->next = malloc(sizeof(struct listnode));
+    p = p->next;
+    p->elem = 8;
+    p->next = malloc(sizeof(struct listnode));
+    p = p->next;
+    p->elem = 3;
+    p->next = malloc(sizeof(struct listnode));
+    p = p->next;
+    p->elem = 4;
+    p->next = malloc(sizeof(struct listnode));
+    p = p->next;
+    p->elem = 7;
+    p->next = NULL;
+    
+    q = malloc(sizeof(struct listnode));
+    R = q;
+    q->elem = 5;
+    q->next = malloc(sizeof(struct listnode));
+    q = q->next;
+    q->elem = 3;
+    q->next = malloc(sizeof(struct listnode));
+    q = q->next;
+    q->elem = 7;
+    q->next = NULL;
+    
+    L = filter(L, R);
+    while (L->next != NULL)
+    {
+        printf("%d\n", L->elem);
+        L = L->next;
+    }
+}
